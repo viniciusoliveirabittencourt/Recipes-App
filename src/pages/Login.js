e4a8b4e1623506208e5efd6719b259c88560856d
Olávio Timóteo <olaviotimoteo@hotmail.com>
2021-11-05 17:23:44 -0300

    Resolve requirements 2, 3 and 4

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 function Login() {
+  const [loginState, setLoginState] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (value, id) => {
+    setLoginState({
+      ...loginState,
+      [id]: value,
+    });
+  };
+
+  const { email, password } = loginState;
+
   return (
-    <div>
-      <p>Tela de Login</p>
-    </div>
+    <form>
+      <label htmlFor="email">
+        E-mail: 
+        <input
+          id="email"
+          type="email"
+          data-testid="email-input"
+          value={ email }
+          onChange={ ({ target: { value, id } }) => handleChange(value, id) }
+          placeholder="Insira seu e-mail"
+        />
+      </label>
+      <label htmlFor="password">
+        Senha: 
+        <input
+          id="password"
+          type="password"
+          data-testid="password-input"
+          value={ password }
+          onChange={ ({ target: { value, id } }) => handleChange(value, id) }
+          placeholder="Insira sua senha"
+        />
+      </label>
+      <Button variant="success" data-testid="login-submit-btn">Entrar</Button>
+    </form>
   );
 }
 
