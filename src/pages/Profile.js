729c67a809b564412bbed1c68941553a68ecc404
Olávio Timóteo <olaviotimoteo@hotmail.com>
2021-11-10 15:25:12 -0300

    Adding completed profile screen

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Profile() {
+  const [email, setEmail] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    const userEmail = JSON.parse(localStorage.getItem('user'));
+    if (userEmail === null) {
+      setEmail('Error, email não encontrado');
+    } else {
+      setEmail(userEmail.email);
+    }
+  }, []);
+
+  function handleLogout() {
+    localStorage.clear();
+    history.push('/');
+  }
+
   return (
     <div>
       <Header pagename="Perfil" completeSearch={ false } />
       <p>Tela de Perfil</p>
+      <h4 data-testid="profile-email">{email}</h4>
+      <div>
+        <button
+          data-testid="profile-done-btn"
+          type="button"
+          onClick={ () => history.push('/receitas-feitas') }
+        >
+          Receitas Feitas
+        </button>
+        <button
+          data-testid="profile-favorite-btn"
+          type="button"
+          onClick={ () => history.push('/receitas-favoritas') }
+        >
+          Receitas Favoritas
+        </button>
+        <button
+          data-testid="profile-logout-btn"
+          type="button"
+          onClick={ handleLogout }
+        >
+          Sair
+        </button>
+      </div>
       <Footer />
     </div>
   );
