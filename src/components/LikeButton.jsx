2d9ec26837fdc8e65c500c93f1af5a625d078d81
luizsmatos <gusstavopdm@gmail.com>
2021-11-11 12:49:36 -0300

    Requisito 44 - Concluido

--- src/components/LikeButton.jsx
+++ src/components/LikeButton.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import likeIconImg from '../images/whiteHeartIcon.svg';
-import likeCompleteIconImg from '../images/blackHeartIcon.svg';
+import whiteHeartIcon from '../images/whiteHeartIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 export default function LikeButton({ id }) {
-  const [isFavoriteRecipe, setIsFavoriteRecipe] = useState(false);
+  // const [isFavoriteRecipe, setIsFavoriteRecipe] = useState(false);
   const getFavoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')) || [];
   const isFavorite = getFavoriteRecipes
     .some((favoriteRecipe) => favoriteRecipe.id === id);
-  const srcIconHeart = isFavorite ? likeCompleteIconImg : likeIconImg;
+  const srcIconHeart = isFavorite ? blackHeartIcon : whiteHeartIcon;
   return (
     <button
       type="button"
       data-testid="favorite-btn"
-      onClick={ () => setIsFavoriteRecipe(!isFavoriteRecipe) }
+      // onClick={ () => setIsFavoriteRecipe(!isFavoriteRecipe) }
+      src={ srcIconHeart }
     >
       <img src={ srcIconHeart } alt="Curtir Receita" className="icon-heart" />
     </button>
